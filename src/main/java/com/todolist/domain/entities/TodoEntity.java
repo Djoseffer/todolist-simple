7bc6502ed86f983d520a71c6ee246f2a2ebf573e
Djoseffer <tom_djosefer@hotmail.com>
2024-05-25 14:51:12 -0300

    feat: entity created

--- src/main/java/com/todolist/domain/entities/TodoEntity.java
+++ src/main/java/com/todolist/domain/entities/TodoEntity.java
@@ -1,4 +1,22 @@
 package com.todolist.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizada;
 }
