006696a7f7177375ba2d0f2738f286dba45c85d0
Djoseffer <tom_djosefer@hotmail.com>
2024-05-25 14:52:41 -0300

    feat: repository created

---
+++ src/main/java/com/todolist/repositories/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.todolist.repositories;
+
+import com.todolist.domain.entities.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+}
