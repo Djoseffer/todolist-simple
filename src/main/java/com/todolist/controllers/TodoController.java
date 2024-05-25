c481e7ce2df66122e63dfa22d5e44c6212640540
Djoseffer <tom_djosefer@hotmail.com>
2024-05-25 14:52:24 -0300

    feat: controller http created

---
+++ src/main/java/com/todolist/controllers/TodoController.java
@@ -0,0 +1,36 @@
+package com.todolist.controllers;
+
+import com.todolist.domain.entities.TodoEntity;
+import com.todolist.services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @PostMapping
+    List<TodoEntity> create(@RequestBody TodoEntity todo){
+        return todoService.create(todo);
+    }
+
+    @PutMapping
+    List<TodoEntity> update(@RequestBody TodoEntity todo){
+      return   todoService.update(todo);
+    }
+
+    @GetMapping
+    List<TodoEntity> list(TodoEntity todo){
+       return todoService.list();
+    }
+
+    @DeleteMapping("{id}")
+    List<TodoEntity> delete(@PathVariable("id") Long id){
+       return todoService.delete(id);
+    }
+}
